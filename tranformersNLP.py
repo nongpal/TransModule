f92aa868669890d8a6d3a970288fcf89c968745d
nongpal <naufalhafish666@gmail.com>
2024-05-16 20:13:17 +0700

    tranformersNLP.py

--- tranformersNLP.py
+++ tranformersNLP.py
@@ -50,6 +50,10 @@ class TokenAndPositioningEmbedding(L.Layer):
             input_dim=maxlen, 
             output_dim=embedding_dim
             )
+        
+        self.maxlen = maxlen
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
 
     def call(self, x):
         maxlen = ops.shape(x)[-1]
@@ -57,3 +61,131 @@ class TokenAndPositioningEmbedding(L.Layer):
         positions = self.positional_embedding(positions)
         x = self.token_embedding(x)
         return x + positions
+    
+
+class TransformerEncoder(L.Layer):
+    
+    def __init__(self, embedding_dim, dense_dim, num_heads, name='Transformer_Encoder', **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.embedding_dim = embedding_dim
+        self.dense_dim = dense_dim
+        self.num_heads = num_heads
+        self.attention = L.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embedding_dim
+        )
+        self.dense_proj = sq(
+            [
+                L.Dense(dense_dim, activation=tf.nn.relu),
+                L.Dense(embedding_dim)
+                ]
+            )
+        
+        self.layer_normalization1 = L.LayerNormalization()
+        self.layer_normalization2 = L.LayerNormalization()
+        self.supports_masking = True
+        
+    def call(self, inputs, mask=None):
+        if mask is not None:
+            padding_mask = ops.cast(mask[:, None, :], dtype="int32")
+        else:
+            padding_mask = None
+        
+        attention_output = self.attention(
+            query=inputs, value=inputs, key=inputs, attention_mask=padding_mask
+        )
+        proj_input = self.layer_normalization1(inputs + attention_output)
+        proj_output = self.dense_proj(proj_input)
+        return self.layer_normalization2(proj_input + proj_output)
+    
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "embedding_dim": self.embedding_dim,
+                "dense_dim": self.dense_dim,
+                "num_heads": self.num_heads,
+            }
+        )
+        return config
+    
+
+class TransformerDecoder(L.Layer):
+    
+    def __init__(self, embedding_dim, latent_dim, num_heads, name='Transformer_Decoder', **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.embedding_dim = embedding_dim
+        self.latent_dim = latent_dim
+        self.num_heads = num_heads
+        
+        self.attention_1 = L.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embedding_dim
+        )
+        
+        self.attention_2 = L.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embedding_dim
+        )
+        
+        self.dense_proj = sq(
+            [
+                L.Dense(latent_dim, activation=tf.nn.relu),
+                L.Dense(embedding_dim)
+            ]
+        )
+        
+        self.layer_normalization1 = L.LayerNormalization()
+        self.layer_normalization2 = L.LayerNormalization()
+        self.layer_normalization3 = L.LayerNormalization()
+        self.support_masking = True
+        
+    def call(self, inputs, encoder_outputs, mask=None):
+        causal_mask = self.get_causal_attention_mask(inputs)
+        if mask is not None:
+            padding_mask = ops.cast(mask[:, None, :], dtype="int32")
+            padding_mask = ops.minimum(padding_mask, causal_mask)
+        else:
+            padding_mask=None
+                
+        attention_output_1 = self.attention_1(
+            query=inputs, value=inputs, key=inputs, attention_mask=causal_mask
+        )
+        
+        out_1 = self.layer_normalization1(inputs + attention_output_1)
+        
+        attention_output_2 = self.attention_2(
+            query=out_1,
+            value=encoder_outputs,
+            key=encoder_outputs,
+            attention_mask=padding_mask
+        )
+        
+        out_2 = self.layer_normalization2(out_1 + attention_output_2)
+        
+        proj_output = self.dense_proj(out_2)
+        return self.layer_normalization3(out_2 + proj_output)
+    
+    def get_causal_attention_mask(self, inputs):
+        input_shape = ops.shape(inputs)
+        batch_size, sequence_length = input_shape[0], input_shape[1]
+        i = ops.arange(sequence_length)[:, None]
+        j = ops.arange(sequence_length)
+        mask = ops.cast(i >= j, dtype="int32")
+        mask = ops.reshape(mask, (1, input_shape[1], input_shape[1]))
+        mult = ops.concatenate(
+            [ops.expand_dims(batch_size, -1), ops.convert_to_tensor([1, 1])],
+            axis=0,
+        )
+        return ops.tile(mask, mult)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "embedding_dim": self.embedding_dim,
+                "latent_dim": self.latent_dim,
+                "num_heads": self.num_heads,
+            }
+        )
+        
+        return config
+
+
